3e7314d4684e62cda6db5d7a0b3178f03f6a8d55
Carlos Estrada <carlos.deg02@hotmail.com>
2020-04-27 16:10:34 -0500

    created boilerplate for type checking

--- sem/sem.go
+++ sem/sem.go
@@ -11,8 +11,8 @@ func SemanticCheck(program *ast.Program) (*dir.FuncDirectory, error) {
 
     // Build the function directory and their corresponding Var directiories
     // Errors to check:
-    //  *If a function is declared twice
-    //  *If two parameters in the same function have the same id
+    //  * If a function is declared twice
+    //  * If two parameters in the same function have the same id
     //
 	if err := buildFuncDirProgram(program, funcdir); err != nil {
 		return nil, err
@@ -20,10 +20,24 @@ func SemanticCheck(program *ast.Program) (*dir.FuncDirectory, error) {
     
     // Check the scope of function calls and variable uses.
     // Errors to check:
-    //  *If a function is called that does not exist
-    //  *If a variable is used and it has not been declared in the parameters
+    //  * If a function is called that does not exist
+    //  * If a variable is used and it has not been declared in the parameters
     //
     if err := scopeCheckProgram(program, funcdir); err != nil {
+        return funcdir, err
+    }
+
+    // Check type cohesion
+    // Errors to check:
+    //  * If a function is called and no function in the funcdir match the argument types
+    //  * If the value in the statement of a function does not match its return value
+    //  * Illegal use of use of built-in functions
+    //      - Arithmetic operators: +, -, *, /
+    //      - Relational operators: < , >, equal
+    //      - Logical operatios: (only to be used with bools) and, or, !
+    //  * To check whether a combination of params for an operator is valid, the semantic cube must be consulted
+    //
+    if err := typeCheckProgram(program, funcdir); err != nil {
         return funcdir, err
     }
 
--- sem/typecheck.go
+++ sem/typecheck.go
@@ -1,2 +1,10 @@
 package sem
 
+import (
+	"github.com/Loptt/lambdish-compiler/ast"
+	"github.com/Loptt/lambdish-compiler/dir"
+)
+
+func typeCheckProgram(program *ast.Program, funcdir *dir.FuncDirectory) error {
+	return nil
+}
