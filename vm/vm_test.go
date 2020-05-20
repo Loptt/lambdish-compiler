bd2f76945a31259db3fcd1ed466a88ae41c2d8bd
Carlos Estrada <carlos.deg02@hotmail.com>
2020-05-20 18:23:02 -0500

    Added mergesort example

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -24,7 +24,7 @@ func TestLoadProgram(t *testing.T) {
 
 func TestRunProgram(t *testing.T) {
 	tests := []string{
-		"tests/oddmatrix.obj",
+		"tests/mergesort.obj",
 	}
 
 	for _, test := range tests {
